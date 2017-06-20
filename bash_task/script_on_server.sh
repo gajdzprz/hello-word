6e83b499ff077bfb941aafadd0a11cc376964561
Przemyslaw Gajdzinski <przemyslaw.gajdzinski@tieto.com>
2017-06-20 12:28:08 +0200

    Everything is ok

--- bash_task/script_on_server.sh
+++ bash_task/script_on_server.sh
@@ -4,10 +4,10 @@
 echo "Just initial script";
 
 # cleaning content of file.txt
-echo -n > file.txt;
+echo -n > /home/gajdzprz/git_repo/hello-word/bash_task/file.txt;
 
 # writing to file.txt from 1 to 10 in new line
-for i in {2..10}
+for i in {1..10}
 do
-	echo $i >> file.txt;
+	echo $i >> /home/gajdzprz/git_repo/hello-word/bash_task/file.txt;
 done
